23672bce675a0a8d6d13f7f2bcc6376cf2f860cc
Rohan <rohanpatil9529@gmail.com>
2021-05-27 20:06:23 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var playing = false;
 var score;
+var action;
+var timeremaining;
+var correctAnswer;
 
 //if we click on the start/reset
 document.getElementById("startreset").onclick = function(){
@@ -17,29 +20,111 @@ document.getElementById("startreset").onclick = function(){
 
          //show countdown box
         document.getElementById("scorevalue").innerHTML = score;
-        document.getElementById("timeremaining").style.display = "block";
+        show("timeremaining")
+        timeremaining = 60;
+
+        //hide gameover box
+        hide("gameover")
+
 
         //change button to reset
         document.getElementById("startreset").innerHTML = "Reset Game";
+
+        //Start Countdown
+        startCountdown();
+
+        //generate a new Q&A
+        generateQA();
+
     } 
 }
-         //reload page
-    //if we are not playing
-        //Set score to zero
-        //show countdown box
-        //reduce time by 1sec in loops
-             //timeleft?
-                //yes->continue
-                //no->gameover
-            //change button to reset
-            //generate new Q&A
 
-//if we click on answer box
-    //if we are playing
-        //correct?
-            //yes
-                //increase score
-                //show correct box for for 1sec
-                //generate new question and answers
-            //No
-                //Show try Again for 1 sec
+for(i=1; i<5; i++){
+    document.getElementById("box"+i).onclick = function(){
+        //check if we are palying
+        if (playing == true){
+            if(this.innerHTML == correctAnswer){
+                //correct Answer
+                score++;
+                document.getElementById("scorevalue").innerHTML = score;
+                //Show Correct box and hide wrong box
+                show("correct");
+                hide("wrong");
+                setTimeout(function(){
+                    hide("correct");
+                },1000)
+    
+                //Generate new QA
+                generateQA()
+            }
+            else{
+                //Wrong Answer
+                show("wrong");
+                hide("correct");
+                setTimeout(function(){
+                    hide("wrong");
+                },1000)
+            }
+        }
+    }
+}
+//Functions
+
+//Start Counter
+function startCountdown(){
+    action = setInterval(function(){
+            timeremaining -= 1;
+            document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+            if(timeremaining == 0){
+                stopCountdown();
+                show("gameover");
+                document.getElementById("gameover").innerHTML = "<p>Game Over!</p> <p>Your Score is "+score+".</p>";
+                hide("timeremaining");
+                hide("correct");
+                hide("wrong");
+                playing = false;
+                document.getElementById("startreset").innerHTML = "Start Game";
+            }
+    },1000);
+}
+
+//Stop Counter
+function stopCountdown(){
+    clearInterval(action);
+}
+
+//hide an element
+function hide(id){
+    document.getElementById(id).style.display = "none";
+}
+
+//Show an element
+function show(id){
+    document.getElementById(id).style.display = "block";
+}
+
+function generateQA(){
+    var x = 1 + Math.round(9*Math.random());
+    var y = 1 + Math.round(9*Math.random());
+    correctAnswer = x*y;
+    document.getElementById("question").innerHTML = x + "X" + y;
+    var correctPosition = 1 + Math.round(3*Math.random());
+    document.getElementById("box"+correctPosition).innerHTML = correctAnswer;
+
+
+    var answers = [correctAnswer];
+    //Fill with Wrong Answers 
+    for (i=1;i<5;i++){
+        if(i != correctPosition){
+            //wrong Answer
+            var wrongAnswer
+            do{
+                wrongAnswer = (1 + Math.round(9*Math.random())) + (1 + Math.round(9*Math.random()))
+            }while(answers.indexOf(wrongAnswer)> -1)
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+            answers.push(wrongAnswer);
+        }
+    }
+}
+
+
